f72f17f62cf3d8e7ae1f2d7ebf5abdaae588d394
Ivan Voitovych <col606v@gmail.com>
2024-01-27 05:34:56 +0200

    ResponseBuilder

---
+++ fluffy/Domain/Message/ResponseBuilder.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace Fluffy\Domain\Message;
+
+class ResponseBuilder
+{
+    public array $headers = [];
+    public int $statusCode = 200;
+    public string $statusText = '';
+    public $body = '';
+    public bool $stringify = false;
+
+    static function json($data)
+    {
+        $response = new ResponseBuilder();
+        $response->body = $data;
+        $response->stringify = true;
+        $response->headers['Content-type'] = 'application/json; charset=utf-8';
+        return $response;
+    }
+
+    static function html($data)
+    {
+        $response = new ResponseBuilder();
+        $response->body = $data;
+        $response->headers['Content-type'] = 'text/html; charset=utf-8';
+        return $response;
+    }
+
+    static function text($data)
+    {
+        $response = new ResponseBuilder();
+        $response->body = $data;
+        $response->headers['Content-type'] = 'text/text; charset=utf-8';
+        return $response;
+    }
+
+    static function file($data, $mimeType = null)
+    {
+        $response = new ResponseBuilder();
+        $response->body = $data;
+        if ($mimeType !== null) {
+            $response->headers['Content-type'] = $mimeType;
+        }
+        return $response;
+    }
+
+    function withCode(int $code)
+    {
+        $this->statusCode = $code;
+        return $this;
+    }
+
+    function withHeaders(array $headers)
+    {
+        $this->headers += $headers;
+        return $this;
+    }
+
+    function withBody($data)
+    {
+        $this->body = $data;
+        return $this;
+    }
+
+    function asJson()
+    {
+        $this->stringify = true;
+        $this->headers['Content-type'] = 'application/json; charset=utf-8';
+        return $this;
+    }
+
+    function asPlainText()
+    {
+        $this->stringify = false;
+        $this->headers['Content-type'] = 'text/text; charset=utf-8';
+        return $this;
+    }
+
+    function asHtml()
+    {
+        $this->stringify = false;
+        $this->headers['Content-type'] = 'text/html; charset=utf-8';
+        return $this;
+    }
+
+    function withBodyType(string $bodyType)
+    {
+        $this->headers['Content-type'] = $bodyType;
+        return $this;
+    }
+}
