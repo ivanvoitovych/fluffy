24bcfd4a6f62175a23b52fc61ca14e32e66a32d3
Ivan Voitovych <col606v@gmail.com>
2024-01-27 05:35:01 +0200

    BaseController

--- fluffy/Controllers/BaseController.php
+++ fluffy/Controllers/BaseController.php
@@ -2,39 +2,41 @@
 
 namespace Fluffy\Controllers;
 
+use Fluffy\Domain\Message\ResponseBuilder;
+
 class BaseController
 {
     public function Unauthorized(?string $message = null)
     {
-        return Response::Json([
+        return ResponseBuilder::Json([
             "message" => $message ?? "Unauthorized"
         ])->WithCode(401);
     }
 
     public function Forbidden(?string $message = null)
     {
-        return Response::Json([
+        return ResponseBuilder::Json([
             "message" => $message ?? "Forbidden"
         ])->WithCode(403);
     }
 
     public function NotFound()
     {
-        return Response::Json([
+        return ResponseBuilder::Json([
             "message" => "Not Found"
         ])->WithCode(404);
     }
 
     public function Conflict()
     {
-        return Response::Json([
+        return ResponseBuilder::Json([
             "message" => "Not Found"
         ])->WithCode(409);
     }
 
     public function BadRequest($errors = null)
     {
-        return Response::Json([
+        return ResponseBuilder::Json([
             "message" => "Bad Request",
             "errors" => $errors
         ])->WithCode(400);
@@ -42,7 +44,7 @@ class BaseController
 
     public function TooManyRequests($errors = null)
     {
-        return Response::Json([
+        return ResponseBuilder::Json([
             "message" => "Too Many Requests",
             "errors" => $errors
         ])->WithCode(429);
@@ -50,7 +52,7 @@ class BaseController
 
     public function ServerError($errors = null)
     {
-        return Response::Json([
+        return ResponseBuilder::Json([
             "message" => "Server Error",
             "errors" => $errors
         ])->WithCode(500);
@@ -58,12 +60,12 @@ class BaseController
 
     public function File($content, $mimeType = null)
     {
-        return Response::File($content, $mimeType);
+        return ResponseBuilder::File($content, $mimeType);
     }
 
     public function Redirect(string $location, int $code = 302)
     {
-        return (new Response())
+        return (new ResponseBuilder())
             ->WithHeaders(['Location' => $location])
             ->WithCode($code);
     }
