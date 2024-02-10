cf75bf89d99f9188fc347e2d4b2f181f7e07efd6
Ivan Voitovych <col606v@gmail.com>
2024-02-10 10:27:33 +0200

    BaseEntityMap: property constants

--- fluffy/Data/Entities/BaseEntityMap.php
+++ fluffy/Data/Entities/BaseEntityMap.php
@@ -4,15 +4,21 @@ namespace Fluffy\Data\Entities;
 
 abstract class BaseEntityMap
 {
+    public const PROPERTY_UpdatedOn = 'UpdatedOn';
+    public const PROPERTY_CreatedOn = 'CreatedOn';
+    public const PROPERTY_Id = 'Id';
+
     public static string $Table = '';
     public static string $Schema = 'public';
     public static array $PrimaryKeys = ['Id'];
     public static array $Indexes = [];
     public static array $Ignore = [];
+
     public static function ForeignKeys(): array
     {
         return [];
     }
+
     public static function Columns(): array
     {
         return [
